50b90269a39aabd993efd3671e0e4f6360eafbcd
AtxGamer <101315680+AtxGamer@users.noreply.github.com>
2022-03-10 13:03:07 +0530

    Create HelloTeady.java

---
+++ HelloTeady.java
@@ -0,0 +1,10 @@
+class HelloTeddyScritp {
+    public static void main(String[] args) {
+        int a;
+        for(a=0; a<=5; a++){
+        System.out.println("\\ /");
+        System.out.println("(..)");
+        System.out.println("(\uE022)");
+        }
+    }
+}
